7db4a6a8ccdf87a6be839c2050532c80fb4ac101
andre-savedra <andresavedracruz@gmail.com>
2022-03-15 20:42:05 -0300

    env

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,8 +4,8 @@ import Providers from "next-auth/providers";
 export default NextAuth({
     providers: [
         Providers.GitHub({
-            clientId: '475b61c947125182ca2c',
-            clientSecret: 'c5c1fff50d367b849bf147148b7a80a91165971b',
+            clientId: process.env.CLIENT_ID,
+            clientSecret: process.env.CLIENT_SECRET,
             scope: 'read:user'
         })
     ]
